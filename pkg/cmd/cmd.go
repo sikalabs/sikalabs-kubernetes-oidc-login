732f2d9b3f435b95d8f151899f7f0816bff970a1
Ondrej Sika <ondrej@ondrejsika.com>
2026-03-05 18:28:27 +0100

    feat(pkg/cmd): Add cobra command sikalabs-kubernetes-oidc-login

---
+++ pkg/cmd/cmd.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"github.com/sikalabs/sikalabs-kubernetes-oidc-login/pkg/login"
+	"github.com/spf13/cobra"
+)
+
+var FlagIssuer string
+var FlagClientID string
+
+func GetCmd() *cobra.Command {
+	var Cmd = &cobra.Command{
+		Use:   "sikalabs-kubernetes-oidc-login",
+		Short: "Perform OIDC login and output Kubernetes ExecCredential for kubectl",
+		Args:  cobra.NoArgs,
+		Run: func(c *cobra.Command, args []string) {
+			login.Login(FlagIssuer, FlagClientID)
+		},
+	}
+	Cmd.Flags().StringVar(&FlagIssuer, "oidc-issuer-url", "", "OIDC Issuer URL")
+	Cmd.MarkFlagRequired("oidc-issuer-url")
+	Cmd.Flags().StringVar(&FlagClientID, "oidc-client-id", "", "OIDC Client ID")
+	Cmd.MarkFlagRequired("oidc-client-id")
+	return Cmd
+}
